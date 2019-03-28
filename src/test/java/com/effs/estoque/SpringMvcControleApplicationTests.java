499e3d27709c6153d3b88824c997fb3e494d4f04
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-03-28 00:25:35 -0300

    create project

---
+++ src/test/java/com/effs/estoque/SpringMvcControleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.effs.estoque;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcControleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
