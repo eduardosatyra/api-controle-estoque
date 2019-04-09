185ebe6dee7adf5c23106254faa18bc9b6effbf8
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-04-08 23:37:23 -0300

    add swagger e swagger-ui

--- src/main/java/com/effs/estoque/config/SecurityConfig.java
+++ src/main/java/com/effs/estoque/config/SecurityConfig.java
@@ -37,7 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private JWTUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
-			"/h2-console/**"
+			"/h2-console/**",
+			"/swagger-ui.html",
+			"/swagger-resources/**",
+			"/v2/api-docs",
+			"/webjars/**"
 	};
 	
 	private static final String[] PUBLIC_MATCHERS_GET = {
